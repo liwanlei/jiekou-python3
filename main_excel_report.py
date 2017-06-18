1c17b35d994a86f1ec21e00a67ca0cb9337d0c44
leileili126@163.com <leileili126@163.com>
2017-06-18 13:52:56 +0800

    加入了多线程

--- main_excel_report.py
+++ main_excel_report.py
@@ -7,10 +7,10 @@
 @time: 2017/6/9 12:45
 """
 from  Public.pyreport_excel import create
-import unittest,os
+import unittest,os,threading,datetime
 from testCase.test_case import Testinface
 from Public.emmail import  sendemali
-if __name__ =='__main__':
+def start():
     suite = unittest.TestSuite()
     suite.addTest(Testinface("testinterface"))
     me=Testinface()
@@ -19,4 +19,14 @@ if __name__ =='__main__':
     if os.path.exists(filepath) is False:
         os.system(r'touch %s' % filepath)
     create(filepath,list_fail=list_fail, list_pass=list_pass, list_json=list_json, listurls=listurls, listkeys=listkeys,listconeents=listconeents, listfangshis=listfangshis, listqiwangs=listqiwangs, listids=listids, listrelust=listrelust, listnames=listnames)
-    sendemali(filepath)
+    # sendemali(filepath)
+def testThread():
+    st = datetime.datetime.now()
+    m = threading.Thread(target=start, args=())
+    m.run()
+    end = datetime.datetime.now()
+    print(end - st)
+
+
+if __name__ == '__main__':
+    testThread()
--- main_html.py
+++ main_html.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Author  : leizi
-import unittest,os,datetime
+import unittest,os,datetime,time
 from testCase.test_case import Testinface
 from Public.py_Html import createHtml
 from  Public.emmail import  sendemali
-if __name__ == '__main__':
+import  threading
+def stast():
     starttime=datetime.datetime.now()
     suite = unittest.TestSuite()
     suite.addTest(Testinface("testinterface"))
@@ -15,5 +16,16 @@ if __name__ == '__main__':
         os.system(r'touch %s' % filepath)
     endtime=datetime.datetime.now()
     createHtml(titles='接口测试报告',filepath=filepath,starttime=starttime,endtime=endtime,passge=list_pass,fail=list_fail,id=listids,name=listnames,key=listkeys,coneent=listconeents,url=listurls,meth=listfangshis,yuqi=listqiwangs,json=list_json,relusts=listrelust)
-    sendemali(filepath)
+    # sendemali(filepath)
+def testThread():
+    st=datetime.datetime.now()
+    m=threading.Thread(target=stast,args=())
+    m.run()
+    end=datetime.datetime.now()
+    print(end-st)
+if __name__ == '__main__':
+    testThread()
+
+
+
 
