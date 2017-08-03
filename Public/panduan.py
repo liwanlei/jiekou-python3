523b6348819620dc7f131ce748f9c048cda405e1
leileili126@163.com <leileili126@163.com>
2017-08-03 20:08:15 +0800

    youhua

--- Public/panduan.py
+++ Public/panduan.py
@@ -5,8 +5,8 @@ def assert_in(asserqiwang,fanhuijson):
     if len(asserqiwang.split('=')) > 1:
         data = asserqiwang.split('&')
         result = dict([(item.split('=')) for item in data])
-        value1=([(int(fanhuijson[key])) for key in result.keys()])
-        value2=([(int(value)) for value in result.values()])
+        value1=([(str(fanhuijson[key])) for key in result.keys()])
+        value2=([(str(value)) for value in result.values()])
         if value1==value2:
             return  'pass'
         else:
