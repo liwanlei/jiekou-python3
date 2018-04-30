ee3372e29b584247748478a9c55d85d001f300d4
liwanlei <leileili126@163.com>
2018-04-30 14:04:31 +0800

    优化测试报告

---
+++ Public/get_excel_new.py
@@ -0,0 +1,17 @@
+""" 
+@author: lileilei
+@file: get_excel_new.py 
+@time: 2018/4/30 11:04 
+"""
+import xlrd
+def datacel(filrpath):
+    all_case=[]
+    file=xlrd.open_workbook(filrpath)
+    me=file.sheets()[0]
+    nrows=me.nrows
+    for i in range(1,nrows):
+        all_case.append({"id":me.cell(i,0).value,'key':me.cell(i,2).value,
+                         'coneent':me.cell(i,3).value,'url':me.cell(i,4).value,
+                         'name':me.cell(i,1).value,'fangshi':me.cell(i,5).value,
+                         'assert':me.cell(i,6).value})
+    return all_case
--- run_http_html.py
+++ run_http_html.py
@@ -16,12 +16,12 @@ def start_interface_html_http():
     if os.path.exists(filepath) is False:
         os.system(r'touch %s' % filepath)
     endtime=datetime.datetime.now()
-    createHtml(titles='http接口自动化测试报告',filepath=filepath,starttime=starttime,
+    createHtml(titles=u'http接口自动化测试报告',filepath=filepath,starttime=starttime,
                endtime=endtime,passge=list_pass,fail=list_fail,
                id=listid,name=listname,key=listkey,coneent=listconeent,url=listurl,meth=listfangshi,
                yuqi=listqiwang,json=list_json,relusts=listrelust,weizhi=list_weizhi,exceptions=list_exption)
-    contec = 'http接口自动化测试完成，测试通过:%s,测试失败：%s，异常:%s,未知错误：%s,详情见：%s' % (
+    contec = u'http接口自动化测试完成，测试通过:%s,测试失败：%s，异常:%s,未知错误：%s,详情见：%s' % (
     list_pass, list_fail, list_exption, list_weizhi, filepath)
-    send_ding(content=contec)
+#    send_ding(content=contec)
 if __name__ == '__main__':
     start_interface_html_http()
