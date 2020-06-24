ff21f98cd15071913fe6f472de1d25e8ccff232c
liwanlei <leileili126@163.com>
2020-06-24 09:07:36 +0800

    Update get_excel.py

--- Public/get_excel.py
+++ Public/get_excel.py
@@ -32,3 +32,12 @@ def datacel(filrpath):
     except Exception as e:
         LOG.info('打开测试用例失败，原因是:%s' % e)
         return
+@logger('生成数据驱动所用数据')
+def makedata():
+    listid, listkey, listconeent, listurl, listfangshi, listqiwang, listname=datacel()
+    i=0
+    make_data=[]
+    for i in range(len(listid)):
+        make_data.append({'url':listurl[i],'key':listkey[i],'coneent':listconeent[i],'fangshi':listfangshi[i],'qiwang':listqiwang[i]})
+        i+=1
+    return make_data
